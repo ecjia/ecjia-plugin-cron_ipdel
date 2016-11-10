b5574d92cf759a2df77d709025fba968f9b1f2b6
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-10 09:30:37 +0800

    更新计划任务插件

--- cron_ipdel.class.php
+++ cron_ipdel.class.php
@@ -19,24 +19,17 @@ class cron_ipdel extends cron_abstract
         return array();
     }
     
-    public function get_prepare_data() {
-        $predata = array(
-            'cron_code'     => $this->configure['cron_code'],
-            'cron_name'     => $this->configure['cron_name'],
-//         	'pay_online'   => $this->configure['pay_account_info'],
-        );
+    /**
+     * 计划任务执行方法
+     */
+    public function run() {
         
-        return $predata;
-    }
-    
-    public function notify() {
-    	 
-        return;
-    }
-    
-    public function response() {
-    	 
-        return;
+        empty($cron['ipdel_day']) && $cron['ipdel_day'] = 7;
+        
+        $deltime = gmtime() - $cron['ipdel_day'] * 3600 * 24;
+        $sql = "DELETE FROM " . $ecs->table('stats') .
+        "WHERE  access_time < '$deltime'";
+        $db->query($sql);    
     }
 
 }
